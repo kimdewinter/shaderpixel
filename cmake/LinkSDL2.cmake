6102eeb0dd237e7aec0ad809a99de92704139a61
Kimthulhu <kdewinter87@protonmail.com>
2022-11-08 22:00:12 +0100

    feat: set up build system with SDL2

---
+++ cmake/LinkSDL2.cmake
@@ -0,0 +1,18 @@
+include(FetchContent)
+
+macro(LinkSDL2 TARGET ACCESS)
+	FetchContent_Declare(
+		sdl2
+		GIT_REPOSITORY https://github.com/libsdl-org/SDL
+		GIT_TAG release-2.24.2
+	)
+
+	FetchContent_GetProperties(sdl2)
+
+	if (NOT sdl2_POPULATED)
+		FetchContent_Populate(sdl2)
+	endif()
+
+	target_include_directories(${TARGET} ${ACCESS} ${sdl2_SOURCE_DIR}/include)
+	target_link_libraries(${TARGET} ${ACCESS} ${SDL2_LIBRARIES})
+endmacro()
